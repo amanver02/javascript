7676354fcb1817517527b3e7595059085914fe7c
amanver02 <developeraman02@gmail.com>
2024-07-12 21:46:56 +0530

    first commit

---
+++ helloworld.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+console.log("hello aman")
+console.log("hello amity")
+
+//variables
+a=10;
+console.log(a)
+
+//increment and decrement
+a++;
+console.log(a)
+a--;
+console.log(a)
+
+age=20;
+console.log("my age is ",age)
+
+
