0d7295f386fc9c13947e8a72c4f0f58117c7112a
amanver02 <developeraman02@gmail.com>
2024-07-14 14:56:18 +0530

    #

---
+++ conditionals/age.js
@@ -0,0 +1,6 @@
+age =23;
+if (age>=18){
+    console.log("adult")
+}
+else
+console.log("child")
---
+++ conditionals/rating.js
@@ -0,0 +1,12 @@
+rating =3;
+// 1 - poor
+// 2-  below  average
+// 3 - average
+// 4- good
+// 5 - excellent
+
+if(rating==1) console.log("poor")
+    else if(rating==2) console.log("below average")
+else if(rating==3) console.log("average")
+    else if(rating==4) console.log("good")
+else if(rating==5) console.log("excellent")
