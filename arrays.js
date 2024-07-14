932a72f8b77a7e6dbea1c1ffb90c8d268d949b44
amanver02 <developeraman02@gmail.com>
2024-07-14 23:01:10 +0530

    #

---
+++ arrays.js
@@ -0,0 +1,20 @@
+arr =[2,4,5,3,4];
+console.log(arr);
+arr.push(9);// add at last
+console.log(arr);;
+arr.push(0);
+console.log(arr);
+arr.pop();// removes last element
+console.log(arr)
+arr.unshift(8,9);  // add first
+console.log(arr);
+arr.shift(); // removes first element   
+console.log(arr)
+// for(let i=0;i<n;i++){
+//     console.log(arr[i]);
+// }
+// console.log(arr[3])
+
+//c++ : vectors
+
+
---
+++ arrays2.js
@@ -0,0 +1,4 @@
+const arr = [2,8,3,3];
+arr.push(3);
+console.log(arr)
+
