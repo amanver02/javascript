6e2db47e93f87be5e42fcf9ef8cc501baeb1326d
amanver02 <developeraman02@gmail.com>
2024-08-01 22:19:21 +0530

    #

--- arrays2.js
+++ arrays2.js
@@ -1,4 +1,21 @@
-const arr = [2,8,3,3];
-arr.push(3);
+// const arr = [2,8,3,3];
+// arr.push(3);
+// console.log(arr)
+
+// multiple data types in one array
+
+arr = [[4 , "aman" , 7.8, true]]
 console.log(arr)
 
+// 2D array
+
+array = [[1,2,3,4],  [5,6,7,8]]
+console.log(array)   
+console.log(typeof(array))   
+brr = [6,4,7,8,9,1]
+console.log(brr)
+brr = [3,6,8,9,4,2,1]
+for(let i=0; i<arr.length;i++){
+  brr[i]*= 2;
+}
+console.log(brr);
